a700f3e935c625c79a9e1d554f0c16856f961a71
SebasCoto <secogonzalez2005@gmail.com>
2024-02-20 15:05:39 -1200

    Finalizacion Semana 06

--- src/main/java/com/tienda/service/impl/CategoriaServiceimpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceimpl.java
@@ -9,22 +9,36 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CategoriaServiceimpl implements CategoriaService{
+public class CategoriaServiceimpl implements CategoriaService {
+
     @Autowired
     private CategoriaDao categoriaDao;
 
     @Override
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
-       List<Categoria> lista =categoriaDao.findAll();
-       
-       if(activos){
-           //Remover los elementos inactivos
-           lista.removeIf(c -> !c.isActivo()); // "->" sirve para ponerle un alias a algo, en este caso lista es Categoria pero con "->" es c
-       }
-       
-       return lista;
+        var lista = categoriaDao.findAll();
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
+        }
+        return lista;
     }
 
-    
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
 }
